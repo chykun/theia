7d97ebe59962bcda3cdb87cf8ff75a1798b30bdb
Akos Kitta <kittaakos@typefox.io>
2020-04-08 09:19:24 +0200

    Added a sample.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- examples/api-samples/src/browser/api-samples-frontend-module.ts
+++ examples/api-samples/src/browser/api-samples-frontend-module.ts
@@ -14,11 +14,40 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { ContainerModule } from 'inversify';
+import { ContainerModule, inject, injectable } from 'inversify';
 import { bindDynamicLabelProvider } from './label/sample-dynamic-label-provider-command-contribution';
 import { bindSampleUnclosableView } from './view/sample-unclosable-view-contribution';
+import { CommandRegistry, CommandContribution } from '@theia/core/lib/common/command';
+import { OutputChannelManager } from '@theia/output/lib/common/output-channel';
 
 export default new ContainerModule(bind => {
     bindDynamicLabelProvider(bind);
     bindSampleUnclosableView(bind);
+    bind(CommandContribution).to(LogToOutputChannelCommandContribution).inSingletonScope();
 });
+
+@injectable()
+class LogToOutputChannelCommandContribution implements CommandContribution {
+
+    private timer?: number;
+
+    @inject(OutputChannelManager)
+    private readonly ocm: OutputChannelManager;
+
+    registerCommands(registry: CommandRegistry): void {
+        registry.registerCommand({ id: 'log-to-output-channel', label: 'API Sample: Log to Output' }, {
+            execute: () => {
+                if (this.timer) {
+                    clearInterval(this.timer);
+                } else {
+                    this.timer = window.setInterval(() => {
+                        const channel = this.ocm.getChannel('API Sample');
+                        channel.setVisibility(true);
+                        channel.appendLine(`${Date.now()}`);
+                    }, 1000);
+                }
+            }
+        });
+    }
+
+}
